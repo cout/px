9b6aa649a08707a5b43dc2c3b673aca6fb333e1f
Paul Brannan <curlypaul924@gmail.com>
2011-04-10 00:55:52 -0400

    Main now calls scanner and parser.

--- Token.hpp
+++ Token.hpp
@@ -2,9 +2,11 @@
 #define Token__hpp_
 
 struct Token {
-  char const * z;
-  int value;
-  unsigned n;
+  char const * sval;
+  int ival;
+  double dval;
 };
+
+extern Token p_token;
 
 #endif // Token__hpp_
--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
 #include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <iostream>
+
+#include <unistd.h>
+
+#include "Token.hpp"
+#include "scanner.hpp"
 
 void *PParseAlloc(void *(*mallocProc)(size_t));
 void PParseFree(void *p, void (*freeProc)(void*));
+void PParse(void *yyp, int yymajor, Token yyminor);
+
+typedef struct yy_buffer_state * YY_BUFFER_STATE;
+
+namespace
+{
+
+void parse_file(void * parser, std::istream & in)
+{
+  yyFlexLexer lexer(&in);
+  int yv = lexer.yylex(); // TODO: loop?
+  PParse(parser, yv, p_token);
+}
+
+class ParserGuard
+{
+public:
+  ParserGuard()
+    : parser(PParseAlloc(std::malloc))
+  {
+  }
+
+  ~ParserGuard()
+  {
+    PParseFree(parser, std::free);
+  }
+
+public:
+  void * parser;
+};
+
+}
 
 int main()
 {
-  void * parser = PParseAlloc(std::malloc);
-  PParseFree(parser, std::free);
+  ParserGuard pg;
+  parse_file(pg.parser, std::cin);
 }
 
